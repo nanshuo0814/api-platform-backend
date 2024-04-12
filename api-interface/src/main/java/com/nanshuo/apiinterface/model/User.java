bdc5678c177b0cb067b0b058740c0f62370f2784
南烁 <nanshuo.icu@qq.com>
2024-04-12 22:17:55 +0800

    add api-interface 测试用户获取接口调用

---
+++ api-interface/src/main/java/com/nanshuo/apiinterface/model/User.java
@@ -0,0 +1,15 @@
+package com.nanshuo.apiinterface.model;
+
+import lombok.Data;
+
+/**
+ * 用户
+ *
+ * @author nanshuo
+ * @date 2024/04/12 22:10:56
+ */
+@Data
+public class User {
+
+    private String username;
+}
